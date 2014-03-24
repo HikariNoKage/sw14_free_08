273cf7793405e9121909272830f3055ed3209a2b
Gottfried <gottfried.selenko@student.tugraz.at>
2014-03-24 12:50:32 +0100

    [MH][GS] FirstPageTest
    FirstPage GUI and Test

---
+++ HiraKataTest/src/com/example/hirakata/test/FirstPageTest.java
@@ -0,0 +1,16 @@
+package com.example.hirakata.test;
+
+import gui.FirstPage;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class FirstPageTest extends ActivityInstrumentationTestCase2<FirstPage> {
+
+	public FirstPageTest() {
+		super(FirstPage.class);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+
+}
