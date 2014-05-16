dbe4503ccd8582f41e90fd4cc6876de858c25858
Gottfried <gottfried.selenko@student.tugraz.at>
2014-05-16 14:59:03 +0200

    [GS][MH] Get all Drawables
    Put the method for getting all drawable in the application class

--- HiraKata/src/bl/HiraKataApplication.java
+++ HiraKata/src/bl/HiraKataApplication.java
@@ -1,14 +1,67 @@
 package bl;
 
+import java.util.Vector;
+
+import com.example.hirakata.R;
+
 import android.app.Application;
 
 public class HiraKataApplication extends Application {
 
-	String mode = "hira_";		//application mode
-	int numberOfDrawables = 3;  //3 for testing should be 100
-	int indexOfUsedKana = 0;    //index of the actual kana
-	boolean order = true;		//show kana in order		
-	boolean all = true;			//show all kana
+	String mode = "hira_"; // application mode
+	int numberOfDrawables = 0; // 3 for testing should be 100
+	int indexOfUsedKana = 0; // index of the actual kana
+	boolean order = true; // show kana in order
+	boolean all = true; // show all kana
+
+	public Vector<Integer> getAllDrawableResources() {
+
+		Vector<Integer> allPicRes = new Vector<Integer>();
+
+		int resource = 0;
+		int[] noKana = { 12, 13, 15, 23, 24, 34, 35, 37, 45 }; // +46 Katakana
+		String mode = this.mode;
+
+		if (mode.equals("all_")) {
+			resource = 1;
+			numberOfDrawables = 110;
+		} else if (mode.equals("hira_")) {
+			resource = 1;
+			numberOfDrawables = 55;
+		} else if (mode.equals("kata_")) {
+			resource = 56;
+			for (int i = 0; i < noKana.length; i++) {
+				noKana[i] = noKana[i] + 46;
+			}
+			numberOfDrawables = 110;
+		}
+		try {
+			for (int i = resource; i <= numberOfDrawables; i++) {
+				if (!(contains(noKana, i))) {
+					resource = R.drawable.class.getField("kana_" + i).getInt(
+							null);
+					allPicRes.add(resource);
+				}
+			}
+		} catch (Exception e) {
+			return null;
+		}
+		return allPicRes;
+	}
+
+	public boolean contains(final int[] array, final int key) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == key) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Vector<Integer> antiSort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	public boolean isOrder() {
 		return order;
--- HiraKata/src/gui/PracticePage.java
+++ HiraKata/src/gui/PracticePage.java
@@ -27,18 +27,14 @@ public class PracticePage extends Activity implements OnClickListener {
 	@SuppressWarnings("unused")
 	private ImageView iconSmall;
 	private DrawingPanel dpanel;
-	private Vector<Integer> allPicRes;
-	private Vector<Integer> shownKana;
 	private HiraKataApplication application;
 	boolean start = true;
+	private Vector<Integer> allPicRes;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_practice_page);
-
-		allPicRes = new Vector<Integer>();
-		shownKana = new Vector<Integer>();
 
 		this.soundButton = (ImageButton) findViewById(R.id.soundButton);
 		this.backButton = (ImageButton) findViewById(R.id.backButton);
@@ -57,8 +53,10 @@ public class PracticePage extends Activity implements OnClickListener {
 		this.dpanel = (DrawingPanel) findViewById(R.id.drawing);
 		application = ((HiraKataApplication) this.getApplicationContext());
 
-		// getAllDrawableResources();
-		// showKana(this.allPicRes.get(0));
+		allPicRes = application.getAllDrawableResources();
+		if (!application.isOrder()) {
+			allPicRes = application.antiSort();
+		}
 	}
 
 	@Override
@@ -67,33 +65,20 @@ public class PracticePage extends Activity implements OnClickListener {
 		int actualKana = application.getIndexOfUsedKana();
 
 		if (start) {
-			getAllDrawableResources();
-			if (!application.isOrder()) {
-				antiSort();
-			}
 			showKana(this.allPicRes.get(actualKana));
 			start = false;
 		}
-
+		
 		if (view.getId() == R.id.drawButton) {
 			drawKana();
 		} else if (view.getId() == R.id.deleteButton) {
 			showKana(this.allPicRes.get(actualKana));
 		} else if (view.getId() == R.id.nextButton) {
-			this.shownKana.add(actualKana);
 			nextKana(actualKana);
 		} else if (view.getId() == R.id.backButton) {
 			previousKana(actualKana);
 		}
 	};
-
-	public void antiSort() {
-		Random randi = new Random();
-
-		while (this.allPicRes.size() > this.shownKana.size()) {
-
-		}
-	}
 
 	public void drawKana() {
 		this.dpanel.setDrawAble(true);
@@ -103,30 +88,26 @@ public class PracticePage extends Activity implements OnClickListener {
 	}
 
 	public void previousKana(int actualKana) {
-		if (application.isOrder()) {
-			actualKana--;
-			if (actualKana >= 0) {
-				showKana(this.allPicRes.get(actualKana));
-				application.setIndexOfUsedKana(actualKana);
-				this.dpanel.invalidate();
-			} else {
-				Toast.makeText(this, this.getString(R.string.prev_toast),
-						Toast.LENGTH_LONG).show();
-			}
+		actualKana--;
+		if (actualKana >= 0) {
+			showKana(this.allPicRes.get(actualKana));
+			application.setIndexOfUsedKana(actualKana);
+			this.dpanel.invalidate();
+		} else {
+			Toast.makeText(this, this.getString(R.string.prev_toast),
+					Toast.LENGTH_LONG).show();
 		}
 	}
 
 	public void nextKana(int actualKana) {
-		if (application.isOrder()) {
-			actualKana++;
-			if (application.getNumberOfDrawables() > actualKana) {
-				showKana(this.allPicRes.get(actualKana));
-				application.setIndexOfUsedKana(actualKana);
-				this.dpanel.invalidate();
-			} else {
-				Toast.makeText(this, this.getString(R.string.next_toast),
-						Toast.LENGTH_LONG).show();
-			}
+		actualKana++;
+		if (this.allPicRes.size() > actualKana) {
+			showKana(this.allPicRes.get(actualKana));
+			application.setIndexOfUsedKana(actualKana);
+			this.dpanel.invalidate();
+		} else {
+			Toast.makeText(this, this.getString(R.string.next_toast),
+					Toast.LENGTH_LONG).show();
 		}
 	}
 
@@ -136,50 +117,6 @@ public class PracticePage extends Activity implements OnClickListener {
 		this.dpanel.invalidate();
 	}
 
-	public boolean getAllDrawableResources() {
-		this.allPicRes.clear();
-		application = ((HiraKataApplication) this.getApplicationContext());
-		int resource = 0;
-		int[] noKana = { 12, 13, 15, 23, 24, 34, 35, 37, 45 }; // +46 Katakana
-		String mode = application.getMode();
-
-		if (mode.equals("all_")) {
-			resource = 1;
-			application.setNumberOfDrawables(110);
-		} else if (mode.equals("hira_")) {
-			resource = 1;
-			application.setNumberOfDrawables(55);
-		} else if (mode.equals("kata_")) {
-			resource = 56;
-			for (int i = 0; i < noKana.length; i++) {
-				noKana[i] = noKana[i] + 46;
-			}
-			application.setNumberOfDrawables(110);
-		}
-		int maxDrawable = application.getNumberOfDrawables();
-
-		try {
-			for (int i = resource; i <= maxDrawable; i++) {
-				if (!(contains(noKana, i))) {
-					resource = R.drawable.class.getField("kana_" + i).getInt(
-							null);
-					this.allPicRes.add(resource);
-				}
-			}
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
-
-	public boolean contains(final int[] array, final int key) {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == key) {
-				return true;
-			}
-		}
-		return false;
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
