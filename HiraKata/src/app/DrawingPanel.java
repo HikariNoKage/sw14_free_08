f9a8fbcbba6ee38b909a869a2d39ac5ce9fae806
Gottfried <gottfried.selenko@student.tugraz.at>
2014-06-12 15:49:05 +0200

    NewMaster
    Fixed Master

---
+++ HiraKata/src/app/DrawingPanel.java
@@ -0,0 +1,117 @@
+package app;
+
+
+import android.content.Context;
+import android.view.View;
+import android.util.AttributeSet;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+
+public class DrawingPanel extends View {
+
+	private Path dPath;
+	private Paint dPaint, cPaint;
+	private int pColor = 0xFF660000;
+	private Canvas dCanvas;
+	private Bitmap cBitmap;
+	private boolean drawAble = false;
+
+	public DrawingPanel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setupDrawing();
+	}
+
+	private void setupDrawing() {
+		this.dPath = new Path();
+		this.dPaint = new Paint();
+		this.dPaint.setColor(Color.BLUE);
+		this.dPaint.setAlpha(0x80);
+		this.dPaint.setAntiAlias(true);
+		this.dPaint.setStrokeWidth(15);
+		this.dPaint.setStyle(Paint.Style.STROKE);
+		this.dPaint.setStrokeJoin(Paint.Join.ROUND);
+		this.dPaint.setStrokeCap(Paint.Cap.ROUND);
+		this.cPaint = new Paint(Paint.DITHER_FLAG);
+	}
+
+	public void setColor(String newColor) {
+		this.pColor = Color.parseColor(newColor);
+		this.dPaint.setColor(this.pColor);
+	}
+
+	public void setStrokeWidth(int width) {
+		this.dPaint.setStrokeWidth(width);
+	}
+
+	public void setDrawAble(boolean drawAble) {
+		this.drawAble = drawAble;
+	}
+
+	public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+
+		int width = bm.getWidth();
+		int height = bm.getHeight();
+		float scaleWidth = ((float) newWidth) / width;
+		float scaleHeight = ((float) newHeight) / height;
+		Matrix matrix = new Matrix();
+		matrix.postScale(scaleWidth, scaleHeight);
+		Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+				matrix, false);
+		return resizedBitmap;
+	}
+
+	public void newDrawing(Bitmap bitmap) {
+
+		this.dCanvas.drawColor(Color.WHITE);
+
+		this.dCanvas.drawBitmap(
+				getResizedBitmap(bitmap, this.getHeight(), this.getWidth()),
+				getMatrix(), null);
+
+		this.dPath.reset();
+		this.invalidate();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		if (this.drawAble) {
+			float xCoordinate = event.getX();
+			float yCoordinate = event.getY();
+
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				this.dPath.moveTo(xCoordinate, yCoordinate);
+			} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+				this.dPath.lineTo(xCoordinate, yCoordinate);
+			} else if (event.getAction() == MotionEvent.ACTION_POINTER_UP) {
+				this.dCanvas.drawPath(this.dPath, this.dPaint);
+			} else {
+				return false;
+			}
+			this.invalidate();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawBitmap(this.cBitmap, 0, 0, this.cPaint);
+		canvas.drawPath(this.dPath, this.dPaint);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		this.cBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		this.dCanvas = new Canvas(this.cBitmap);
+	}
+
+}
---
+++ HiraKata/src/app/PracticePage.java
@@ -0,0 +1,218 @@
+package app;
+
+import java.util.Map;
+import java.util.Vector;
+import com.example.android.app.R;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PracticePage extends Activity implements OnClickListener {
+
+	private ImageButton soundButton, backButton, drawButton, deleteButton,
+			nextButton;
+
+	private TextView largeText;
+	private ImageView iconSmall;
+	private DrawingPanel dpanel;
+	private HiraKataApplication application;
+	boolean start = true;
+	private Vector<Integer> allPicRes;
+	private Map<Integer, String> names;
+	private Map<Integer, Integer> smallPics;
+	private Map<Integer, Integer> sounds;
+	private MediaPlayer play;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_practice_page);
+
+		this.soundButton = (ImageButton) findViewById(R.id.soundButton);
+		this.backButton = (ImageButton) findViewById(R.id.backButton);
+		this.drawButton = (ImageButton) findViewById(R.id.drawButton);
+		this.deleteButton = (ImageButton) findViewById(R.id.deleteButton);
+		this.nextButton = (ImageButton) findViewById(R.id.nextButton);
+
+		this.soundButton.setOnClickListener(this);
+		this.backButton.setOnClickListener(this);
+		this.drawButton.setOnClickListener(this);
+		this.deleteButton.setOnClickListener(this);
+		this.nextButton.setOnClickListener(this);
+
+		this.largeText = (TextView) findViewById(R.id.lageText);
+		this.iconSmall = (ImageView) findViewById(R.id.iconSmall);
+		this.dpanel = (DrawingPanel) findViewById(R.id.drawing);
+
+		application = ((HiraKataApplication) this.getApplicationContext());
+		allPicRes = new Vector<Integer>();
+
+		if (application.isOrder()) {
+			this.allPicRes.clear();
+			this.allPicRes = application.getAllPicRes();
+			/*
+			 * Log.w("order", "order true: " + application.isOrder() + " size:"
+			 * + allPicRes.size());
+			 */
+		} else {
+			this.allPicRes.clear();
+			this.allPicRes = application.getAllPicResRand();
+			/*
+			 * Log.w("order", "order false: " + application.isOrder() + " size:"
+			 * + allPicRes.size());
+			 */
+		}
+		names = application.getNames();
+		smallPics = application.getPicResSmall();
+		sounds = application.getSounds();
+		// Log.w("Sounds", "sounds: "+ sounds.size());
+	}
+
+	@Override
+	public void onClick(View view) {
+
+		int actualKana = application.getIndexOfUsedKana();
+		// Log.w("actual", "" + actualKana);
+		if (start) {
+			showKana(this.allPicRes.get(actualKana));
+			this.largeText.setText(names.get(this.allPicRes.get(actualKana)));
+			this.iconSmall.setImageResource(smallPics.get(this.allPicRes
+					.get(actualKana)));
+			start = false;
+		}
+
+		
+		
+		if (view.getId() == R.id.drawButton) {
+			drawKana();
+		} else if (view.getId() == R.id.deleteButton) {
+			showKana(this.allPicRes.get(actualKana));
+		} else if (view.getId() == R.id.nextButton) {
+			nextKana(actualKana);
+		} else if (view.getId() == R.id.backButton) {
+			previousKana(actualKana);
+		} else if (view.getId() == R.id.soundButton) {
+			play = MediaPlayer.create(getApplicationContext(),
+					this.sounds.get(this.allPicRes.get(actualKana)));
+
+			AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+			// int volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+			switch (am.getRingerMode()) {
+			case AudioManager.RINGER_MODE_SILENT:
+				play.setVolume(0, 0);
+				break;
+			case AudioManager.RINGER_MODE_VIBRATE:
+				play.setVolume(0, 0);
+				break;
+			case AudioManager.RINGER_MODE_NORMAL:
+				play.setVolume(1, 1);
+				break;
+			}
+
+			this.soundButton.setEnabled(false);
+
+			play.setOnCompletionListener(new OnCompletionListener() {
+
+				@Override
+				public void onCompletion(MediaPlayer mpalmost) {
+					soundButton.setEnabled(true);
+					mpalmost.release();
+				}
+			});
+			play.start();
+		}
+	};
+
+	public void drawKana() {
+		this.dpanel.setDrawAble(true);
+		// this.dpanel.setColor("BLACK");
+		this.dpanel.setStrokeWidth(45);
+		this.dpanel.invalidate();
+	}
+
+	public void previousKana(int actualKana) {
+		// Log.w("prev", "prev: " + actualKana);
+		if (actualKana > 0) {
+			actualKana--;
+			if(this.allPicRes.get(actualKana)==null)
+			{
+				while(this.allPicRes.get(actualKana)==null)
+				{
+					actualKana--;
+				}
+			}
+			this.largeText.setText(names.get(this.allPicRes.get(actualKana)));
+			this.iconSmall.setImageResource(smallPics.get(this.allPicRes
+					.get(actualKana)));
+			showKana(this.allPicRes.get(actualKana));
+			application.setIndexOfUsedKana(actualKana);
+			this.dpanel.invalidate();
+		} else {
+			Toast.makeText(this, this.getString(R.string.prev_toast),
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void nextKana(int actualKana) {
+		// Log.w("next", "next: " + actualKana);
+		if (this.allPicRes.size() > (actualKana + 1)) {
+			actualKana++;
+			if(this.allPicRes.get(actualKana)==null)
+			{
+				while(this.allPicRes.get(actualKana)==null)
+				{
+					actualKana++;
+				}
+			}
+			this.largeText.setText(names.get(this.allPicRes.get(actualKana)));
+			this.iconSmall.setImageResource(smallPics.get(this.allPicRes
+					.get(actualKana)));
+			
+			showKana(this.allPicRes.get(actualKana));
+			application.setIndexOfUsedKana(actualKana);
+			this.dpanel.invalidate();
+		} else {
+			Toast.makeText(this, this.getString(R.string.next_toast),
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void showKana(int id) {
+		Bitmap drawable = BitmapFactory.decodeResource(getResources(), id);
+		this.dpanel.newDrawing(drawable);
+		this.dpanel.invalidate();
+	}
+
+	@Override
+	public void onBackPressed() {
+		application.setIndexOfUsedKana(0);
+		this.finish();
+		return;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.practice_page, menu);
+		return true;
+	}
+
+}
